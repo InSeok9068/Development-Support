d976a16c6d4a62590d28ece60dd8478e5c8d6b65
Lee In Seok <dlstjr9068@gmail.com>
2023-04-11 23:55:38 +0900

    @Injectable() 정의

--- backend/src/translation/google.translation.ts
+++ backend/src/translation/google.translation.ts
@@ -1,5 +1,7 @@
 import { Translation } from './interfaces/translation.interface';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class GoogleTranslation implements Translation {
   getTranslatedCharacters(word: string): string {
     const googleApiKey = process.env.GOOGLE_API_KEY;
--- backend/src/translation/naver.translation.ts
+++ backend/src/translation/naver.translation.ts
@@ -1,5 +1,7 @@
 import { Translation } from './interfaces/translation.interface';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class NaverTranslation implements Translation {
   getTranslatedCharacters(word: string): string {
     return word;
