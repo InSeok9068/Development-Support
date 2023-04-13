400742f37b864d48cf4b68d3b8b85331a2cd949c
Lee In Seok <dlstjr9068@gmail.com>
2023-04-13 17:19:34 +0900

    db 연동 및 환경변수 설정

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,10 +1,11 @@
 import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
 import * as path from 'path';
 
 @Controller()
 export class AppController {
   @Get()
-  root(@Res() res) {
+  root(@Res() res: Response) {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
   }
 }
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,19 +1,33 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { TranslationModule } from './translation/translation.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    // SPA 페이지 로드
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    // 컨피그 전역 설정
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    // DB 설정
+    TypeOrmModule.forRoot({
+      type: 'sqlite', // - DB 종류
+      database: 'db/DEVELOPMENT-SUPPORT.db', // - DB 파일 이름
+      autoLoadEntities: true, // - 구동시 entity파일 자동 로드
+      synchronize: process.env.NODE_ENV == 'dev' ? true : false, // - 서비스 구동시 entity와 디비의 테이블 싱크 개발만 할것
+      dropSchema: process.env.NODE_ENV == 'dev' ? true : false, // - 구동시 해당 테이블 삭제 synchronize와 동시 사용
+      logging: true, // - orm 사용시 로그 남기기
+    }),
     TranslationModule,
+    UsersModule,
   ],
   controllers: [AppController],
 })
