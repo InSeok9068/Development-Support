ea48f2301c6c1da31af0a399557ab3311d0cd189
Lee In Seok <dlstjr9068@gmail.com>
2023-04-14 18:36:54 +0900

    인터셉터 적용

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { LoggingInterceptor } from './logging/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // 스웨거 설정
   const config = new DocumentBuilder()
     .setTitle('Development Support')
     .setDescription('개발 도우미')
@@ -13,6 +15,9 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  // 인터셉터 전역 설정
+  app.useGlobalInterceptors(new LoggingInterceptor());
 
   await app.listen(4000);
 }
