c533d795b14018d7e609281695cec477ad39a1fa
Lee In Seok <dlstjr9068@gmail.com>
2023-04-23 00:23:34 +0900

    프론트엔드 어드민 페이지 프로젝트 초기화

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,39 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineApplicationConfig } from '@vben/vite-config';
 
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+export default defineApplicationConfig({
+  overrides: {
+    optimizeDeps: {
+      include: [
+        'echarts/core',
+        'echarts/charts',
+        'echarts/components',
+        'echarts/renderers',
+        'qrcode',
+        '@iconify/iconify',
+        'ant-design-vue/es/locale/zh_CN',
+        'ant-design-vue/es/locale/en_US',
+      ],
+    },
+    server: {
+      proxy: {
+        '/basic-api': {
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+          ws: true,
+          rewrite: (path) => path.replace(new RegExp(`^/basic-api`), ''),
+          // only https
+          // secure: false
+        },
+        '/upload': {
+          target: 'http://localhost:3300/upload',
+          changeOrigin: true,
+          ws: true,
+          rewrite: (path) => path.replace(new RegExp(`^/upload`), ''),
+        },
+      },
+    },
+    build: {
+      outDir: '../backend/public',
+    },
   },
-  build: {
-    outDir: '../backend/public'
-  }
-})
+});
