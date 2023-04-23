bebd88e73e3042b36f0dcd04c55328108939b696
Lee In Seok <dlstjr9068@gmail.com>
2023-04-23 10:38:52 +0900

    영어가 기본으로 실행되도록 진행 및 배포 스크립트 변경

--- frontend/src/settings/localeSetting.ts
+++ frontend/src/settings/localeSetting.ts
@@ -4,26 +4,31 @@ import type { LocaleSetting, LocaleType } from '/#/config';
 export const LOCALE: { [key: string]: LocaleType } = {
   ZH_CN: 'zh_CN',
   EN_US: 'en',
+  KO_KR: 'ko',
 };
 
 export const localeSetting: LocaleSetting = {
   showPicker: true,
   // Locale
-  locale: LOCALE.ZH_CN,
+  locale: LOCALE.EN_US,
   // Default locale
-  fallback: LOCALE.ZH_CN,
+  fallback: LOCALE.EN_US,
   // available Locales
-  availableLocales: [LOCALE.ZH_CN, LOCALE.EN_US],
+  availableLocales: [LOCALE.EN_US, LOCALE.KO_KR, LOCALE.ZH_CN],
 };
 
 // locale list
 export const localeList: DropMenu[] = [
   {
-    text: '简体中文',
-    event: LOCALE.ZH_CN,
-  },
-  {
     text: 'English',
     event: LOCALE.EN_US,
+  },
+  {
+    text: '한국어',
+    event: LOCALE.KO_KR,
+  },
+  {
+    text: '简体中文',
+    event: LOCALE.ZH_CN,
   },
 ];
