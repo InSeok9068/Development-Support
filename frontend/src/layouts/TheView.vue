fdbbce629e471a5d4a2c66bd19eada577c247920
Lee In Seok <dlstjr9068@gmail.com>
2023-08-21 23:56:54 +0900

    레이아웃 추가

---
+++ frontend/src/layouts/TheView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="wrap">
+    <!--가변으로 변경되는 페이지 영역-->
+    <!-- <RouterView :key="$route.fullPath"></RouterView> -->
+    <router-view v-slot="{ Component }">
+      <component :is="Component" />
+    </router-view>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
