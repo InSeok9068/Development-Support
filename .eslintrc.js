8d3947f28db1c2bec39f9c5736ff967364590421
Lee In Seok <dlstjr9068@gmail.com>
2023-09-05 16:48:54 +0900

    eslint 루트경로 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const eslintBaseConfig = require('@support/eslint-config-custom/base');
-
 module.exports = {
-  ...eslintBaseConfig,
-  env: {
-    node: true,
-    browser: true,
-  },
-  extends: ['eslint:recommended', ...eslintBaseConfig.extends],
+  ...require('@support/eslint-config-custom/base'),
 };
