eecf37922460dba143c461f0d2327477d6b69bcf
Lee In Seok <dlstjr9068@gmail.com>
2023-09-08 11:09:26 +0900

    prettier 전역 설정

--- packages/eslint-config-custom/base.js
+++ packages/eslint-config-custom/base.js
@@ -1,15 +1,15 @@
 module.exports = {
   root: true,
   env: {},
-  plugins: ["@typescript-eslint"],
-  extends: ["plugin:@typescript-eslint/recommended", "prettier"],
+  plugins: ['@typescript-eslint'],
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
   parserOptions: {
-    ecmaVersion: "2022",
-    sourceType: "module",
-    parser: "@typescript-eslint/parser",
+    ecmaVersion: '2022',
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser',
   },
   rules: {
-    "@typescript-eslint/no-unused-vars": "off", //사용하지 않는 변수 경고 끄기
-    "@typescript-eslint/no-explicit-any": "off", //any 사용시 경고 끄기
+    '@typescript-eslint/no-unused-vars': 'off', //사용하지 않는 변수 경고 끄기
+    '@typescript-eslint/no-explicit-any': 'off', //any 사용시 경고 끄기
   },
 };
