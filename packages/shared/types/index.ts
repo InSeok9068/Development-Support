85e7b4c5e4bb35c2aba38271b36aa99d010f3c98
Lee In Seok <dlstjr9068@gmail.com>
2023-09-10 10:24:42 +0900

    codegen 설정

--- packages/shared/types/index.ts
+++ packages/shared/types/index.ts
@@ -1,9 +1,30 @@
-import { z } from 'zod';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string };
+  String: { input: string; output: string };
+  Boolean: { input: boolean; output: boolean };
+  Int: { input: number; output: number };
+  Float: { input: number; output: number };
+};
 
-export const User = z.object({
-  id: z.number(),
-  userId: z.string(),
-  password: z.string(),
-  username: z.string(),
-  email: z.string().email(),
-});
+export type Query = {
+  __typename?: 'Query';
+  user?: Maybe<User>;
+  users?: Maybe<Array<Maybe<User>>>;
+};
+
+export type User = {
+  __typename?: 'User';
+  email: Scalars['String']['output'];
+  id: Scalars['Int']['output'];
+  password: Scalars['String']['output'];
+  userId: Scalars['String']['output'];
+  username: Scalars['String']['output'];
+};
