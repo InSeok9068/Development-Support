8f8de500b299eb2aaced2476698d1232a36c6042
Lee In Seok <dlstjr9068@gmail.com>
2023-09-12 18:43:37 +0900

    Mutation 작업완료

--- apps/backend/src/graphql/resolvers/today.work.resolver.ts
+++ apps/backend/src/graphql/resolvers/today.work.resolver.ts
@@ -1,3 +1,5 @@
+import { DeleteTodayWorkInput, MutationCreateTodayWorkArgs } from '@support/shared/types';
+import dayjs from 'dayjs';
 import { prisma } from '../../configs';
 
 const resolvers = {
@@ -8,11 +10,39 @@ const resolvers = {
   },
 
   Mutation: {
-    // createTodayWork: ({ title, tag, content }: Work) => {
-    //   prisma.work.create({
-    //     data: { title, tag },
-    //   });
-    // },
+    createTodayWork: async (_: any, args: MutationCreateTodayWorkArgs) => {
+      const now = dayjs();
+      const work = await prisma.work.create({
+        data: {
+          title: args.input.title,
+          tag: args.input.tag,
+          day: now.get('D'),
+          month: now.get('M'),
+          year: now.get('y'),
+          week: now.get('d'),
+        },
+      });
+      const workItem = await prisma.workItem.create({
+        data: {
+          workId: work.id,
+          content: args.input.content,
+        },
+      });
+
+      return !!workItem && !!work;
+    },
+    deleteTodayWork: (input: DeleteTodayWorkInput) => {
+      prisma.work.delete({
+        where: {
+          id: Number(input.id),
+        },
+      });
+      prisma.workItem.deleteMany({
+        where: {
+          workId: Number(input.id),
+        },
+      });
+    },
   },
 };
 
