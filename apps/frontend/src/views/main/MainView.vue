0fdf8b07889991d09f104f9d17b9fd6c0c94760d
Lee In Seok <dlstjr9068@gmail.com>
2023-09-12 13:30:28 +0900

    불필요한 코드 삭제

--- apps/frontend/src/views/main/MainView.vue
+++ apps/frontend/src/views/main/MainView.vue
@@ -4,30 +4,11 @@
 </template>
 
 <script setup lang="ts">
-// import { ApolloClient, InMemoryCache, gql } from '@apollo/client/core';
-
 import { apolloClient } from '@/composables/use.grahpql.client';
 import { gql } from '@apollo/client/core';
 import { provideApolloClient, useQuery } from '@vue/apollo-composable';
 
 const apiTest = () => {
-  // const apolloClient = new ApolloClient({
-  //   uri: import.meta.env.VITE_GRAPHQL_URL,
-  //   cache: new InMemoryCache(),
-  // });
-
-  // apolloClient
-  //   .query({
-  //     query: gql`
-  //       query users {
-  //         user {
-  //           username
-  //         }
-  //       }
-  //     `,
-  //   })
-  //   .then((result) => console.log(result));
-
   const { result } = provideApolloClient(apolloClient)(() =>
     useQuery(gql`
       query users {
