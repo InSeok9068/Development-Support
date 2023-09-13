98fc3e96a5f80041c4e7bfa30bb731484417edf0
Lee In Seok <dlstjr9068@gmail.com>
2023-09-13 17:01:18 +0900

    불필요 제네릭 제거

--- apps/frontend/src/views/main/MainView.vue
+++ apps/frontend/src/views/main/MainView.vue
@@ -6,16 +6,11 @@
 <script setup lang="ts">
 import { apolloClient } from '@/composables/use.grahpql.client';
 import { gql } from '@apollo/client/core';
-import { type User } from '@support/shared/types';
 import { provideApolloClient, useQuery } from '@vue/apollo-composable';
-
-interface Data<T> {
-  user: T;
-}
 
 const apiTest = () => {
   const { result } = provideApolloClient(apolloClient)(() =>
-    useQuery<Data<User>>(gql`
+    useQuery(gql`
       query users {
         user {
           username
