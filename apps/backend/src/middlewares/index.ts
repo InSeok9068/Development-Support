678060ab7aba160db4dd935f693f05938cf7a489
Lee In Seok <dlstjr9068@gmail.com>
2023-11-01 18:22:01 +0900

    1분에 100회 API 요청 제한 설정

--- apps/backend/src/middlewares/index.ts
+++ apps/backend/src/middlewares/index.ts
@@ -1,5 +1,6 @@
 import { authMiddleware } from './auth.middleware';
 import { errorMiddleware } from './error.middleware';
+import { limiterMiddleware } from './limiter.middleware';
 import { morganMiddleware } from './moran.middleware';
 
-export { authMiddleware, errorMiddleware, morganMiddleware };
+export { authMiddleware, errorMiddleware, limiterMiddleware, morganMiddleware };
