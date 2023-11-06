fd9565eea0f744412beb637b3434dd2ccc2d45d9
Lee In Seok <dlstjr9068@gmail.com>
2023-11-07 00:32:12 +0900

    로그인 페이지 정리

--- apps/frontend/src/views/login/LoginView.vue
+++ apps/frontend/src/views/login/LoginView.vue
@@ -4,6 +4,7 @@
       <div class="py-24">
         <h2 class="mb-20 text-center text-2xl font-semibold">로그인</h2>
         <div id="firebaseui-auth-container"></div>
+        <div id="loader">Loading...</div>
       </div>
     </div>
   </div>
@@ -17,16 +18,19 @@ import 'firebaseui/dist/firebaseui.css';
 import { onMounted } from 'vue';
 
 const uiConfig = {
-  signInSuccessUrl: '<url-to-redirect-to-on-success>',
+  callbacks: {
+    uiShown: () => {
+      document.getElementById('loader')!.style.display = 'none';
+    },
+  },
+  signInSuccessUrl: '/main', // 성공 시 리다이렉트 URL
   signInOptions: [
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     firebase.auth.FacebookAuthProvider.PROVIDER_ID,
     firebase.auth.GithubAuthProvider.PROVIDER_ID,
   ],
-  tosUrl: '<your-tos-url>',
-  privacyPolicyUrl: function () {
-    window.location.assign('<your-privacy-policy-url>');
-  },
+  tosUrl: '<your-tos-url>', // 서비스이용약관
+  privacyPolicyUrl: () => window.location.assign('<your-privacy-policy-url>'), // 개인정보처리방침
 };
 
 const ui = firebaseui.auth.AuthUI.getInstance() ?? new firebaseui.auth.AuthUI(auth);
