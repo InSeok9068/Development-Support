909c91e37e200b337786f3c10cd24f7902148a3c
Lee In Seok <dlstjr9068@gmail.com>
2023-11-10 12:32:07 +0900

    대시보드 예시 파일 커밋

--- apps/frontend/src/views/dashboard/DashboardView.vue
+++ apps/frontend/src/views/dashboard/DashboardView.vue
@@ -1,3 +1,35 @@
-<template>대시보드</template>
+<template>
+  <div style="width: 800px"><canvas id="acquisitions"></canvas></div>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Chart from 'chart.js/auto';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  (async function () {
+    const data = [
+      { year: 2010, count: 10 },
+      { year: 2011, count: 20 },
+      { year: 2012, count: 15 },
+      { year: 2013, count: 25 },
+      { year: 2014, count: 22 },
+      { year: 2015, count: 30 },
+      { year: 2016, count: 28 },
+    ];
+
+    new Chart(document.getElementById('acquisitions') as HTMLCanvasElement, {
+      type: 'bar',
+      data: {
+        labels: data.map((row) => row.year),
+        datasets: [
+          {
+            label: 'Acquisitions by year',
+            data: data.map((row) => row.count),
+          },
+        ],
+      },
+    });
+  })();
+});
+</script>
