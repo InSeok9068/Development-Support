14582941eb8d7305303a7e6bb04e23807fd9a5fa
Lee In Seok <dlstjr9068@gmail.com>
2023-12-08 19:07:27 +0900

    Prisma Mock 테스트 환경 구축

--- apps/backend/src/__tests__/sample.test.ts
+++ apps/backend/src/__tests__/sample.test.ts
@@ -1,5 +1,24 @@
-import { expect, test } from 'vitest';
+import { work } from '@/services/today.work.service';
+import { test, vi } from 'vitest';
+import { prisma } from '../configs/__mocks__/prisma.config';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3);
+vi.mock('../configs/prisma.config');
+
+test('한일 조회', async () => {
+  const resultWork = {
+    id: 1,
+    title: '테스트 데이터',
+    year: 2023,
+    month: 12,
+    date: 20231231,
+    week: 4,
+    time: 1,
+    tag: null,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
+  prisma.work.findUniqueOrThrow.mockResolvedValue(resultWork);
+
+  console.log(await work(1));
 });
