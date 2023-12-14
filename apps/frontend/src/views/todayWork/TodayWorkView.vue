2bc1a02a804752069966b4f4cbcaba15302149d7
Lee In Seok <dlstjr9068@gmail.com>
2023-12-15 08:28:42 +0900

    오늘 할일 페이지 코드 완료

--- apps/frontend/src/views/todayWork/TodayWorkView.vue
+++ apps/frontend/src/views/todayWork/TodayWorkView.vue
@@ -1,65 +1,66 @@
 <template>
-  <div class="form-control join w-2/6">
-    <label class="label">
-      <span class="label-text">오늘 무슨 일을 하셨나요?</span>
-    </label>
-    <input v-model="todayWorkInputArgs.title" class="input input-bordered" type="text" placeholder="제목" />
-    <div class="mt-1"></div>
-    <textarea v-model="todayWorkInputArgs.content" class="textarea textarea-bordered" placeholder="내용"></textarea>
-    <div class="mt-3"></div>
-    <input v-model="todayWorkInputArgs.time" type="range" min="1" max="8" class="range range-info" step="1" />
-    <div class="flex justify-between px-2 text-xs">
-      <span v-for="(item, index) in 8" :key="index">{{ item }}시간</span>
+  <div class="grid">
+    <div class="col-12 md:col-6">
+      <div class="card">
+        <div class="flex flex-column gap-2">
+          <label>오늘 무슨 일을 하셨나요?</label>
+          <InputText v-model="todayWorkInputArgs.title" type="text" placeholder="제목" />
+        </div>
+        <div class="mt-1"></div>
+        <Textarea v-model="todayWorkInputArgs.content" auto-resize rows="5" cols="30" placeholder="내용" />
+        <div class="mt-3"></div>
+        <input v-model="todayWorkInputArgs.time" type="range" min="1" max="8" class="range range-info" step="1" />
+        <div class="flex justify-between px-2 text-xs">
+          <span v-for="(item, index) in 8" :key="index">{{ item }}시간</span>
+        </div>
+        <div class="mt-3"></div>
+        <Button label="등록" @click.prevent.stop="onClickRegist()"></Button>
+      </div>
     </div>
-    <div class="mt-3"></div>
-    <button class="btn btn-primary w-1/3" @click.prevent.stop="onClickRegist()">등록</button>
-  </div>
-  <div class="form-control join float-right w-3/6">
-    <div>
-      <b class="text-2xl">오늘 한일</b>
-      <input
-        v-model="todayWorkSearchArgs.date"
-        type="date"
-        class="m-2 w-min rounded-xl border border-black pl-1"
-        @change="works(todayWorkSearchArgs)"
-      />
-      <input type="month" class="m-2 hidden w-min rounded-xl border border-black pl-1" />
-      <input type="week" class="m-2 hidden w-min rounded-xl border border-black pl-1" />
-      <button class="btn btn-neutral float-right" @click.prevent.stop="onSendWeeklyReport()">주간보고서 전송</button>
+    <div class="col-12 md:col-6">
+      <div class="card flex align-items-center flex-column md:flex-row gap-3">
+        <b>오늘 한일</b>
+        <Calendar v-model="todayWorkSearchArgs.date" show-icon @date-select="works(todayWorkSearchArgs)" />
+        <Button label="주간보고서 전송" @click.prevent.stop="onClickRegist()"></Button>
+      </div>
+      <div class="mt-1"></div>
+      <ul>
+        <li
+          v-for="(item, index) in todayWorkListArgs.item"
+          :key="index"
+          class="mb-2"
+          @drop.prevent="onDrop($event, item.id)"
+          @dragenter.prevent
+          @dragover.prevent
+        >
+          <span class="text-lg font-bold">{{ `- ${item.title} ` }}</span>
+          <span class="text-lg font-bold text-red-700">{{ `[${item.time}H]` }}</span>
+          <ul class="ml-3">
+            <li
+              v-for="(subItem, subIndex) in item.subItem"
+              :key="subIndex"
+              draggable="true"
+              @dragstart="onDragStart($event, subItem.id)"
+            >
+              <!-- <InputGroup>
+                <Button icon="pi pi-check" severity="success" />
+                <InputText placeholder="Vote" />
+                <Button icon="pi pi-times" severity="danger" />
+              </InputGroup> -->
+              <InputGroup>
+                <InputGroupAddon>{{ `${subIndex + 1}) ${subItem.content} ` }}</InputGroupAddon>
+                <InputGroupAddon>{{ `[${subItem.time}H]` }}</InputGroupAddon>
+                <Button icon="pi pi-eraser" text rounded @click.prevent.stop="onClickItemRemove(subItem.id)" />
+              </InputGroup>
+            </li>
+          </ul>
+        </li>
+      </ul>
     </div>
-    <div class="mt-1"></div>
-    <ul>
-      <li
-        v-for="(item, index) in todayWorkListArgs.item"
-        :key="index"
-        class="mb-2"
-        @drop.prevent="onDrop($event, item.id)"
-        @dragenter.prevent
-        @dragover.prevent
-      >
-        <span class="text-lg font-bold">{{ `- ${item.title} ` }}</span>
-        <span class="text-lg font-bold text-red-700">{{ `[${item.time}H]` }}</span>
-        <ul class="ml-3">
-          <li
-            v-for="(subItem, subIndex) in item.subItem"
-            :key="subIndex"
-            draggable="true"
-            @dragstart="onDragStart($event, subItem.id)"
-          >
-            <span>{{ `${subIndex + 1}) ${subItem.content} ` }}</span>
-            <span class="text-lg font-bold text-red-500">{{ `[${subItem.time}H]` }}</span>
-            <button class="btn btn-square btn-ghost" @click.prevent.stop="onClickItemRemove(subItem.id)">
-              <TrashIcon />
-            </button>
-          </li>
-        </ul>
-      </li>
-    </ul>
   </div>
 </template>
 
 <script setup lang="ts">
-import TrashIcon from '@/components/icon/TrashIcon.vue';
 import { useTodayWork } from '@/composables/todayWork/today.work';
 import type { CreateTodayWorkItemInput, UpdateTodayWorkItemForTransferInput } from '@support/shared/types';
 import { onMounted } from 'vue';
