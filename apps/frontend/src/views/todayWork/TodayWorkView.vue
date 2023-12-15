baadaa7aeec0bd1ac54dcf6ec3c773ec6bd6ca39
Lee In Seok <dlstjr9068@gmail.com>
2023-12-16 00:30:36 +0900

    오늘 한일 페이지 디자인 적용

--- apps/frontend/src/views/todayWork/TodayWorkView.vue
+++ apps/frontend/src/views/todayWork/TodayWorkView.vue
@@ -7,11 +7,11 @@
           <InputText v-model="todayWorkInputArgs.title" type="text" placeholder="제목" />
         </div>
         <div class="mt-1"></div>
-        <Textarea v-model="todayWorkInputArgs.content" auto-resize rows="5" cols="30" placeholder="내용" />
+        <Textarea v-model="todayWorkInputArgs.content" class="md:col-12" auto-resize rows="5" placeholder="내용" />
         <div class="mt-3"></div>
-        <input v-model="todayWorkInputArgs.time" type="range" min="1" max="8" class="range range-info" step="1" />
-        <div class="flex justify-between px-2 text-xs">
-          <span v-for="(item, index) in 8" :key="index">{{ item }}시간</span>
+        <Slider v-model="todayWorkInputArgs.time" class="md:w-26rem mb-2" :min="1" :max="8" :step="1" />
+        <div class="flex align-items-center flex-column md:flex-row gap-6">
+          <span v-for="(item, index) in 8" :key="index">{{ item }}</span>
         </div>
         <div class="mt-3"></div>
         <Button label="등록" @click.prevent.stop="onClickRegist()"></Button>
@@ -24,38 +24,46 @@
         <Button label="주간보고서 전송" @click.prevent.stop="onClickRegist()"></Button>
       </div>
       <div class="mt-1"></div>
-      <ul>
-        <li
-          v-for="(item, index) in todayWorkListArgs.item"
-          :key="index"
-          class="mb-2"
-          @drop.prevent="onDrop($event, item.id)"
-          @dragenter.prevent
-          @dragover.prevent
-        >
-          <span class="text-lg font-bold">{{ `- ${item.title} ` }}</span>
-          <span class="text-lg font-bold text-red-700">{{ `[${item.time}H]` }}</span>
-          <ul class="ml-3">
-            <li
-              v-for="(subItem, subIndex) in item.subItem"
-              :key="subIndex"
-              draggable="true"
-              @dragstart="onDragStart($event, subItem.id)"
-            >
-              <!-- <InputGroup>
+      <div class="card">
+        <ul>
+          <li
+            v-for="(item, index) in todayWorkListArgs.item"
+            :key="index"
+            class="mb-2"
+            @drop.prevent="onDrop($event, item.id)"
+            @dragenter.prevent
+            @dragover.prevent
+          >
+            <span class="text-lg font-bold">{{ `${item.title} ` }}</span>
+            <span class="text-lg font-bold text-red-700">{{ `[${item.time}H]` }}</span>
+            <ul class="ml-3">
+              <li
+                v-for="(subItem, subIndex) in item.subItem"
+                :key="subIndex"
+                draggable="true"
+                @dragstart="onDragStart($event, subItem.id)"
+              >
+                <!-- <InputGroup>
                 <Button icon="pi pi-check" severity="success" />
                 <InputText placeholder="Vote" />
                 <Button icon="pi pi-times" severity="danger" />
               </InputGroup> -->
-              <InputGroup>
-                <InputGroupAddon>{{ `${subIndex + 1}) ${subItem.content} ` }}</InputGroupAddon>
-                <InputGroupAddon>{{ `[${subItem.time}H]` }}</InputGroupAddon>
-                <Button icon="pi pi-eraser" text rounded @click.prevent.stop="onClickItemRemove(subItem.id)" />
-              </InputGroup>
-            </li>
-          </ul>
-        </li>
-      </ul>
+                <InputGroup>
+                  <InputGroupAddon>{{ `${subIndex + 1}) ${subItem.content} ` }}</InputGroupAddon>
+                  <InputGroupAddon>{{ `[${subItem.time}H]` }}</InputGroupAddon>
+                  <Button
+                    class="vertical-align-baseline"
+                    icon="pi pi-eraser"
+                    text
+                    rounded
+                    @click.prevent.stop="onClickItemRemove(subItem.id)"
+                  />
+                </InputGroup>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
