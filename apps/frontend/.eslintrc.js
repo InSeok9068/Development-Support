e072e567dd13f679b7bfe3212ba075b6cf9e3137
Lee In Seok <dlstjr9068@gmail.com>
2023-12-29 11:16:48 +0900

    eslint 옵션 통일

--- apps/frontend/.eslintrc.js
+++ apps/frontend/.eslintrc.js
@@ -9,8 +9,7 @@ module.exports = {
   parser: 'vue-eslint-parser',
   extends: ['plugin:vue/vue3-recommended', ...eslintBaseConfig.extends],
   rules: {
-    '@typescript-eslint/no-unused-vars': 'off', //사용하지 않는 변수 경고 끄기
-    '@typescript-eslint/no-explicit-any': 'off', //any 사용시 경고 끄기
+    ...eslintBaseConfig.rules,
     'vue/multi-word-component-names': 'off',
     'vue/no-reserved-component-names': 'off',
   },
