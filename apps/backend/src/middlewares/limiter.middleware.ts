14f629b6352a4244429e291ed71e04c2741f2bb5
Lee In Seok <dlstjr9068@gmail.com>
2024-01-19 18:41:16 +0900

    API 요청 제한 문자열 변경

--- apps/backend/src/middlewares/limiter.middleware.ts
+++ apps/backend/src/middlewares/limiter.middleware.ts
@@ -4,7 +4,7 @@ const limiterMiddleware = rateLimit({
   windowMs: 60 * 1000, // 1분
   limit: 100, // 100회
   handler(_req, res, _next, _option) {
-    res.status(200).json({ errors: [{ message: 'API 요청 제한' }] });
+    res.status(200).json({ errors: [{ message: 'API 요청 제한 잠시 후 다시 시도해주세요.' }] });
   },
 });
 
