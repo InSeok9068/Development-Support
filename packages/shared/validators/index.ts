9d294f27f7432310b9f6c4f01abcfc01644a1968
Lee In Seok <dlstjr9068@gmail.com>
2024-01-19 17:49:15 +0900

    zod 수정

--- packages/shared/validators/index.ts
+++ packages/shared/validators/index.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
 export const CreateTodayWorkItemInputValidator = z.object({
-  content: z.string().nonempty(),
+  content: z.string().min(1),
   tag: z.string().optional(),
-  title: z.string().nonempty(),
+  title: z.string().min(1),
   time: z.number().min(1).max(8),
 });
