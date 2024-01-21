bcfe9e0566ddb5381f4050a67b2413757aa96153
Lee In Seok <dlstjr9068@gmail.com>
2024-01-21 16:35:41 +0900

    스크래핑 임시 작업

--- apps/backend/playground/backend.palyground.ts
+++ apps/backend/playground/backend.palyground.ts
@@ -5,6 +5,15 @@ const delay = (milliseconds: number) => new Promise((resolve) => setTimeout(reso
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto('https://pms.kpcard.co.kr/projects/palrago/issues');
+  await page.goto('https://news.hada.io');
+  await delay(500);
+  console.log(
+    await page.$$eval('.topictitle > a', (el) =>
+      el.map((el) => ({
+        title: el.textContent,
+        link: el.getAttribute('href'),
+      })),
+    ),
+  );
   await browser.close();
 })();
