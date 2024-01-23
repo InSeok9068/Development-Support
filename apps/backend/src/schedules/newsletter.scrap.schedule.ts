a42a8be10bb5b46ea95e508b04b38832d7b0bee2
Lee In Seok <dlstjr9068@gmail.com>
2024-01-23 13:27:13 +0900

    puppeteer 실행 시 옵션 설정

--- apps/backend/src/schedules/newsletter.scrap.schedule.ts
+++ apps/backend/src/schedules/newsletter.scrap.schedule.ts
@@ -7,7 +7,7 @@ const newsletterScrapAction = async () => {
   for (const source of ['GeekNews']) {
     switch (source) {
       case 'GeekNews': {
-        const browser = await puppeteer.launch({ headless: 'new' });
+        const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox', '--disable-setuid-sandbox'] });
         const page = await browser.newPage();
         await page.goto('https://news.hada.io');
         await delay(500);
