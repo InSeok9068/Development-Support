838a670f5e1398f90e620a28bfc4f6493be37468
Lee In Seok <dlstjr9068@gmail.com>
2024-01-25 15:41:54 +0900

    바인딩 오류 수정

--- apps/frontend/src/composables/namer/namer.ts
+++ apps/frontend/src/composables/namer/namer.ts
@@ -1,7 +1,7 @@
 import { useApolloQuery } from '@/composables/use.apollo.query';
 import { NAME_SUGGESTIONS_QUERY } from '@/graphql/operations/namer.operation';
 import type { UiNamerFormArgs } from '@/ui/namer.ui';
-import type { QueryNameSuggestionsArgs } from '@support/shared/types';
+import type { NameSuggestionsQuery, QueryNameSuggestionsArgs } from '@support/shared/types';
 import { ref } from 'vue';
 
 const useNamer = () => {
@@ -15,12 +15,12 @@ const useNamer = () => {
   const nameSuggestionsArgs = ref<string>();
 
   const nameSuggestions = async (namerFormArgs: UiNamerFormArgs) => {
-    const { apolloQuery } = useApolloQuery<string, QueryNameSuggestionsArgs>(NAME_SUGGESTIONS_QUERY, {
+    const { apolloQuery } = useApolloQuery<NameSuggestionsQuery, QueryNameSuggestionsArgs>(NAME_SUGGESTIONS_QUERY, {
       input: namerFormArgs,
     });
 
     const result = await apolloQuery();
-    nameSuggestionsArgs.value = result.data;
+    nameSuggestionsArgs.value = result.data.nameSuggestions;
   };
 
   return {
