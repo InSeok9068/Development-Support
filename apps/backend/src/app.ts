a2daa12415d691360d4603b996cca8e000c036e1
Lee In Seok <dlstjr9068@gmail.com>
2024-02-08 14:56:27 +0900

    1. dotenv 초기파일 업로드 2. 패키지 일괄 업데이트 진행

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,6 +1,8 @@
 /* dotenv Setting */
 import dotenv from 'dotenv';
 import path from 'path';
+
+dotenv.config();
 dotenv.config({ path: path.join(__dirname, '..', `.env.${process.env.NODE_ENV}`) });
 /* dotenv Setting */
 
