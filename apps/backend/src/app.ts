0849238e666c539ae7a1bd551538c04e3a21a270
Lee In Seok <dlstjr9068@gmail.com>
2024-02-08 18:05:28 +0900

    dotenv 설정 제거

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -2,7 +2,6 @@
 import dotenv from 'dotenv';
 import path from 'path';
 
-dotenv.config();
 dotenv.config({ path: path.join(__dirname, '..', `.env.${process.env.NODE_ENV}`) });
 /* dotenv Setting */
 
