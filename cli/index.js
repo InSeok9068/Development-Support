a28d1b32bffb60e7ad0a4635df1c089a68ff1380
Lee In Seok <dlstjr9068@gmail.com>
2024-02-27 15:17:31 +0900

    파일 생성 CLI 생성

--- cli/index.js
+++ cli/index.js
@@ -1,14 +1,11 @@
 #!/usr/bin/env node
 
+import { checkbox, confirm, input, select } from '@inquirer/prompts';
 import { program } from 'commander';
 import figlet from 'figlet';
 import fs from 'fs-extra';
-import inquirer from 'inquirer';
-import inquirerPrompt from 'inquirer-autocomplete-prompt';
 
 console.log(figlet.textSync('Development Support CLI'));
-
-inquirer.registerPrompt('autocomplete', inquirerPrompt);
 
 const fileActions = [
   {
@@ -72,79 +69,54 @@ const fileActions = [
 program
   .version('1.0.0')
   .description('개발 도우미 CLI')
-  .action(() => {
-    inquirer
-      .prompt([
+  .action(async () => {
+    const action = await select({
+      message: '무엇을 도와드릴까요?',
+      choices: [
         {
-          type: 'list',
-          name: 'action',
-          message: '무엇을 도와드릴까요?',
-          choices: [
-            {
-              name: '파일 생성',
-              value: 'CREATE_FILE',
-            },
-          ],
+          name: '파일 생성',
+          value: 'CREATE_FILE',
         },
-        {
-          type: 'input',
-          name: 'fileName',
-          message: '파일 이름을 입력하세요.',
-          validate: (input) => {
-            if (input.length === 0) {
-              return '파일 이름을 입력하세요.';
-            }
-            return true;
-          },
-        },
-      ])
-      .then((answers) => {
-        const fileName = answers.fileName;
-        inquirer
-          .prompt([
-            {
-              type: 'checkbox',
-              name: 'option',
-              message: '생성 옵션을 선택해주세요.(다중 선택 가능)',
-              choices: [
-                {
-                  name: 'Backend + Fronted (API 공급)',
-                  value: 1,
-                  checked: true,
-                },
-                {
-                  name: 'Fronted (화면)',
-                  value: 2,
-                },
-                {
-                  name: 'Fronted (라우터)',
-                  value: 3,
-                },
-              ],
-            },
-          ])
-          .then((answers) => {
-            const filterFileActions = fileActions.filter((fileAction) => answers.option.includes(fileAction.option));
-            filterFileActions.forEach((a) => console.log(a.action(fileName)));
+      ],
+    });
 
-            inquirer
-              .prompt([
-                {
-                  name: 'ok',
-                  message: '해당 경로로 파일을 생성하시겠습니까?',
-                  type: 'confirm',
-                },
-              ])
-              .then((answers) => {
-                if (answers.ok) {
-                  filterFileActions.forEach((fileAction) => {
-                    fs.ensureFile(fileAction.action(fileName));
-                  });
-                  console.log('파일 생성이 완료되었습니다.');
-                }
-              });
-          });
+    const fileName = await input({
+      message: '파일 이름을 입력하세요.',
+      validate: (value) => value.length !== 0,
+    });
+
+    const option = await checkbox({
+      message: '생성 옵션을 선택해주세요.(다중 선택 가능)',
+      choices: [
+        {
+          name: 'Backend + Fronted (API 공급)',
+          value: 1,
+          checked: true,
+        },
+        {
+          name: 'Fronted (화면)',
+          value: 2,
+        },
+        {
+          name: 'Fronted (라우터)',
+          value: 3,
+        },
+      ],
+    });
+
+    const filterFileActions = fileActions.filter((fileAction) => option.includes(fileAction.option));
+    filterFileActions.forEach((a) => console.log(a.action(fileName)));
+
+    const ok = await confirm({
+      message: '해당 경로로 파일을 생성하시겠습니까?',
+    });
+
+    if (ok) {
+      filterFileActions.forEach((fileAction) => {
+        fs.ensureFile(fileAction.action(fileName));
       });
+      console.log('파일 생성이 완료되었습니다.');
+    }
   });
 
 program.parse(process.argv);
