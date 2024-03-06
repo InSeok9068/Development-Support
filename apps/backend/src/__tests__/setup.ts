46d7eb7b4daf9d4be3b72a022ce61df314f3ec57
Lee In Seok <dlstjr9068@gmail.com>
2024-03-06 12:50:38 +0900

    테스트 파일 수정 진행

--- apps/backend/src/__tests__/setup.ts
+++ apps/backend/src/__tests__/setup.ts
@@ -2,8 +2,8 @@ import dotenv from 'dotenv';
 import path from 'path';
 
 dotenv.config({ path: path.join(__dirname, '../../', '.env.development') });
-process.env.DATABASE_URL = 'file:../db/DEVELOPMENT-SUPPORT-TEST.db';
 
+// process.env.DATABASE_URL = 'file:../db/DEVELOPMENT-SUPPORT-TEST.db';
 /*
 실행 전 기존 테스트가 생성했던 데이터들 삭제
 npx prisma migrate reset 고려
