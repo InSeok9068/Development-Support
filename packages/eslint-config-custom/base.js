30ae04f8eb1ea5015aa3494fa3f4930194ac3e1d
Lee In Seok <dlstjr9068@gmail.com>
2024-04-06 16:37:54 +0900

    전역으로는 eslint 기본 체크만 진행

--- packages/eslint-config-custom/base.js
+++ packages/eslint-config-custom/base.js
@@ -1,29 +1,3 @@
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-// module.exports = {
-//   root: true,
-//   env: {
-//     es6: true,
-//     node: true,
-//     browser: true,
-//   },
-//   plugins: ['@typescript-eslint'],
-//   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-//   parserOptions: {
-//     ecmaVersion: '2022',
-//     sourceType: 'module',
-//     parser: '@typescript-eslint/parser',
-//   },
-//   rules: {
-//     '@typescript-eslint/no-unused-vars': 'off', //사용하지 않는 변수 경고 끄기
-//     '@typescript-eslint/no-explicit-any': 'off', //any 사용시 경고 끄기
-//     '@typescript-eslint/ban-types': 'off', //빈 타입 경고 끄기
-//   },
-// };
-
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
